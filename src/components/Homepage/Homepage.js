4799b26bd5aa9d997bb4fc144c78fb8558b8edac
kristenmoran <kristen.moran30@gmail.com>
2020-07-07 18:08:33 -0400

    header added

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 function Homepage() {
-	return <div>Hello from Homepage</div>;
+	return (
+	<>
+	<Header></Header>
+	<div>Hello from Homepage</div>
+	</>
+	)
 }
 
 export default Homepage;
