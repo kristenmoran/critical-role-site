f9bec7b93fd7c5db194d19890335a1847b727cb2
kristenmoran <kristen.moran30@gmail.com>
2020-07-15 17:49:05 -0400

    menu now slides in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,21 @@ import './Header.css';
 
 function Header() {
 	const [isToggled, setToggled] = useState(false);
+	const navClassNames = isToggled ? 'nav-links nav-active' : 'nav-links';
+	
 
-	let menuClick = () => setToggled(!isToggled);
+	// let menuClick = (event) => { 
+	// 	setToggled(!isToggled)
+	// 	console.log(isToggled);
+	// 	console.log(event.target);
+	// };
 
 	return (
 		<nav>
 			<div className='logo'>
 				<h4>Critical Role</h4>
 			</div>
-			<ul className='nav-links'>
+			<ul className={navClassNames}>
 				<li>
 					<a href='#'>Videos</a>
 				</li>
@@ -25,7 +31,7 @@ function Header() {
 					<a href='#'>Blog</a>
 				</li>
 			</ul>
-			<div className='burger' onClick={menuClick} open={isToggled}>
+			<div className='burger' onClick={() => setToggled(!isToggled)}>
 				<div className='line1'></div>
 				<div className='line2'></div>
 				<div className='line3'></div>
